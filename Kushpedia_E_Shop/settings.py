c8871db4ad5ce97bac2900e2dd184ef5f538c6d3
Henry Wanjiru <haronkuria2015@gmail.com>
2024-08-17 10:46:31 +0300

    r

--- Kushpedia_E_Shop/settings.py
+++ Kushpedia_E_Shop/settings.py
@@ -162,4 +162,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kushpedia2023py@gmail.com'
-EMAIL_HOST_PASSWORD = 'vaobxgmynxhtiixw'
+EMAIL_HOST_PASSWORD = 'xbskfupdrvkxnaig'
